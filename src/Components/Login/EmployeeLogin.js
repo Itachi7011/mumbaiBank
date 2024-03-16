0ea974a0e9cf3b71795065f7c04354c6378d5804
Itachi7011 <127921762+Itachi7011@users.noreply.github.com>
2024-03-16 11:36:26 +0530

    Update EmployeeLogin.js

--- src/Components/Login/EmployeeLogin.js
+++ src/Components/Login/EmployeeLogin.js
@@ -1,89 +1,99 @@
-import {useState} from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../App";
 import axios from "axios";
 
 const EmployeesLogin = () => {
+  const { state, dispatch } = useContext(UserContext);
 
-    const {state, dispatch} = useContext(UserContext);
+  const [Data, setData] = useState({ post: [] });
 
-    const [Data, setData] = useState({ post: [] });
+  useEffect(() => {
+    axios
+      .get("/empSearchResults")
+      .then((response) => {
+        const data = response.data;
 
-    useEffect(() => {
-        axios.get('/empSearchResults').then((response) => {
+        setData({ post: data });
 
-            const data = response.data;
+        if (data.status === 4000) {
+          console.log("Sorry wrong credentials!");
+        } else {
+          console.log("Login Successful.");
 
+          //dispatch function here
+          dispatch({ type: "USER", payload: false });
+        }
 
-            setData({ post: data });
+        console.log("data fetch successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-            if (data.status === 4000) {
-                console.log("Sorry wrong credentials!")
-            } else {
+  let name, value;
+  const [user, setUser] = useState({ email: "", password: "" });
 
-                console.log("Login Successful.")
+  const handleInput = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
 
-                //dispatch function here
-                dispatch({ type : "USER", payload : false })
+  return (
+    <>
+      <div className="main-body">
+        <div className="nav-btn">
+          <h1>Employees Login (Already Registered Users Only) : </h1>
+          <h3>
+            {" "}
+            If not Registered Yet , Please visit{" "}
+            <a href="/NewEmpRegistration"> Signup Page </a> :{" "}
+          </h3>
 
-               
-            }
+          {/* Form Starts Here */}
+          <form className="loginForm" action="/emplogin" method="POST">
+            <div className="loginCSS">
+              <div class="mb-3 row">
+                <div class="col-sm-10">
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={handleInput}
+                    class="form-control loginFormInput"
+                    autoComplete
+                    placeholder="Email"
+                  />
+                </div>
 
-
-            console.log("data fetch successfully");
-        }).catch((err) => { console.log(err) })
-
-
-    }, [])
-
-    let name, value;
-    const [user, setUser] = useState({ email: "", password: "" });
-
-
-    const handleInput = (e) => {
-        name = e.target.name;
-        value = e.target.value;
-        setUser({ ...user, [name]: value });
-    }
-
-    return(
-        <>
-<div className="main-body mt-5">
-            <div className="nav-btn">
-                <h1>Employees Login (Already Registered Users Only) : </h1>
-                <h3> If not Registered Yet , Please visit <a href="/NewEmpRegistration"> Signup Page </a> : </h3>
-
-                {/* Form Starts Here */}
-            <form action="/emplogin" method="POST">
-                <div className="loginCSS">
-                    <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-10">
-                        <input type="email" name="email" onChange={handleInput} class="form-control" id="inputPassword" placeholder="Please Enter Your Registered Email ID only" />
-                    </div>
-
-                    <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                        <div class="col-sm-10">
-                        <input type="password" name="password" onChange={handleInput} class="form-control" id="inputPassword"/>
-                    </div>
-
-                    
-                    
-                    </div>
-                      
-
-  </div>
-  <button  type="submit" value="signUp" className="btn btn-primary btn-lg m-1" style={{ float: "right" }}>Login</button>
-  </div>
-  
-  </form>
-
+                <div class="mb-3 row">
+                  <div class="col-sm-10">
+                    <input
+                      type="password"
+                      name="password"
+                      onChange={handleInput}
+                      class="form-control loginFormInput"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                value="signUp"
+                className="btn btn-primary btn-lg m-1"
+                style={{ float: "right" }}
+              >
+                Login
+              </button>
             </div>
-            </div>
-        </>
-    )
-}
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default EmployeesLogin;
