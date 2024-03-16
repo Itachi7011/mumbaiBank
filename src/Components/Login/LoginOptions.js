e8f93cdb414f8882d9de39c1624bd23636c2ea87
Itachi7011 <127921762+Itachi7011@users.noreply.github.com>
2024-03-16 11:36:47 +0530

    Update LoginOptions.js

--- src/Components/Login/LoginOptions.js
+++ src/Components/Login/LoginOptions.js
@@ -10,7 +10,7 @@ function LoginOptions() {
 
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/CustomerLogin"> Consumers  </a></button><br/>
 
-                    <button type="button" class="btn btn-primary btn-lg"> <a href="/EmployeeLogin"> Back Employees </a> </button><br/>
+                    <button type="button" class="btn btn-primary btn-lg"> <a href="/EmployeeLogin"> Bank Employees </a> </button><br/>
                     
                     
                 </div>
