a3a695386a7d5dd16d112d2fb72680bce92f342a
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-04-05 14:03:18 +0530

    Update LoanOptions.js

--- src/Components/Loans/LoanOptions.js
+++ src/Components/Loans/LoanOptions.js
@@ -4,7 +4,7 @@ function LoanOptions() {
         <>
             <div className="main-body">
                 <div className="nav-btn">
-                <h1>Please Choose according to your POST :</h1>
+                <h1>Please Choose according to your Account Type :</h1>
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/CustomerSavingLoans"> Saving Accounts </a></button><br/>
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/CustomerCurrentLoans"> Current Accounts </a> </button><br/>
                    
