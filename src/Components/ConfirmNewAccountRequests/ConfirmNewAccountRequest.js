a2ebae11c7f24f83394173a334207e4e12bba307
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-04-14 21:49:26 +0530

    Update ConfirmNewAccountRequest.js

--- src/Components/ConfirmNewAccountRequests/ConfirmNewAccountRequest.js
+++ src/Components/ConfirmNewAccountRequests/ConfirmNewAccountRequest.js
@@ -9,8 +9,7 @@ function ConfirmNewAccountRequest() {
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/ConfirmNewSavingAccountRequest"> New Savings Account Requests  </a></button><br/>
 
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/ConfirmNewCurrentAccountRequest"> New Current Account Requests </a> </button><br/>
-                    
-                    
+                     
                 </div>
             </div>
 
