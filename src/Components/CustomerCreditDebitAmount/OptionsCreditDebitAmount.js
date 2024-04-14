498b563259c3d2258268e51bbef6415bff36795f
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-04-14 21:50:25 +0530

    Update OptionsCreditDebitAmount.js

--- src/Components/CustomerCreditDebitAmount/OptionsCreditDebitAmount.js
+++ src/Components/CustomerCreditDebitAmount/OptionsCreditDebitAmount.js
@@ -7,7 +7,7 @@ function OptionsCreditDebitAmount() {
                 <h1>Please Choose according to your POST :</h1>
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/CustomerSavingCreditDebitAmount"> Saving Accounts </a></button><br/>
                     <button type="button" class="btn btn-primary btn-lg"> <a href="/CustomerCurrentCreditDebitAmount"> Current Accounts </a> </button><br/>
-                   
+                
                 </div>
             </div>
 
