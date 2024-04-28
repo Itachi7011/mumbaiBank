c3382caebee48fbb77c22c497c14c4c367c9dc79
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-04-28 18:42:52 +0530

    Update TechnicalStaffRegistraiton.js

--- src/Components/NewEmpRegistrations/TechnicalStaffRegistraiton.js
+++ src/Components/NewEmpRegistrations/TechnicalStaffRegistraiton.js
@@ -47,23 +47,23 @@ const Scale1Registration = () => {
         <>
             <div className="main-body1">
                 <div className="nav-btn">
-                    <h1> New Employee Registration (Only Scale - I) : </h1>
+                    <h1> New Techinical Staff Registration : </h1>
 
                 </div>
                 <div className="registrationForm">
 
                     <form action="/technicalStaffRegister" method="POST">
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Bank BranchName *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="bankBranch" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Bank BranchName *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="bankBranch" placeholder="" />
                         </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Name *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="name" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Name *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="name" placeholder="" />
                         </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Email (Official Email Id only) *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="email" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Email (Official Email Id only) *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="email" placeholder="" />
                         </div>
 
                         {/* Hidden Scale Input Value : */}
@@ -72,83 +72,83 @@ const Scale1Registration = () => {
                         {/* Hidden Scale Input Value : */}
                         <input type="hidden" name="dateOfFormSubmission" value={Date.now} placeholder="" />
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Father's Name *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="fatherName" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Father's Name *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="fatherName" placeholder="" />
                         </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Date Of Birth *</label>
-                            <input autoComplete="off" onChange={handleInput} type="date" className="form-control" id="exampleFormControlInput1" name="dateOfBirth" placeholder="" />
-                        </div>
-
-
-
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Company's or Agency's Name *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="companyName" placeholder="Full Official Name" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Date Of Birth *</label>
+                            <input autoComplete="off" onChange={handleInput} type="date" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="dateOfBirth" placeholder="" />
                         </div>
 
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Job / Appointment Letter Number *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="JobAppointmentLetterNo" placeholder="" />
-                        </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Aadhar Card Number *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="aadharCardNumber" placeholder="" />
-                        </div>
-
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">P A N (Personal Account Number) *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="panNumber" placeholder="" />
-
-                        <div className="mb-3"> <label for="exampleFormControlInput1" className="form-label">Election Voter Id Number *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="electionVoterIdNo" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Company's or Agency's Name *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="companyName" placeholder="Full Official Name" />
                         </div>
 
 
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Job / Appointment Letter Number *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="JobAppointmentLetterNo" placeholder="" />
                         </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Country (whichever country's citizenship do you have) *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="country" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Aadhar Card Number *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="aadharCardNumber" placeholder="" />
                         </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">State *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="state" placeholder="" />
-                        </div>
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">P A N (Personal Account Number) *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="panNumber" placeholder="" />
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">District *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="district" placeholder="" />
-                        </div>
-
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Full Address *</label>
-                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control" id="exampleFormControlInput1" name="fullAddress" placeholder="" />
+                        <div className="mb-3 formDivRegistraion"> <label for="exampleFormControlInput1" className="form-label">Election Voter Id Number *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="electionVoterIdNo" placeholder="" />
                         </div>
 
 
-                        <div className="mb-3 techStaffRegistrationLabel">
-                            <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">POST during joining *</label>
+                        </div>
+
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Country (whichever country's citizenship do you have) *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="country" placeholder="" />
+                        </div>
+
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">State *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="state" placeholder="" />
+                        </div>
+
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">District *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="district" placeholder="" />
+                        </div>
+
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Full Address *</label>
+                            <input autoComplete="off" onChange={handleInput} type="text" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="fullAddress" placeholder="" />
+                        </div>
+
+
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel">
+                            <label for="exampleFormControlInput1" className="form-label">POST during joining *</label>
                             
-                            <input autoComplete="off" onChange={handleInput} type="number" className="form-control" id="exampleFormControlInput1" name="postDuringJoining" placeholder="" />
+                            <input autoComplete="off" onChange={handleInput} type="number" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="postDuringJoining" placeholder="" />
 
                         </div>
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Basic Salary ( only Basic Salary i.e,. Exluding DA and other addons on salary) *</label>
-                            <input autoComplete="off" onChange={handleInput} type="number" className="form-control" id="exampleFormControlInput1" name="basicSalary" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Basic Salary ( only Basic Salary i.e,. Exluding DA and other addons on salary) *</label>
+                            <input autoComplete="off" onChange={handleInput} type="number" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="basicSalary" placeholder="" />
                         </div>
 
 
-                        <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Date Of Official Joining *</label>
-                            <input autoComplete="off" onChange={handleInput} type="date" className="form-control" id="exampleFormControlInput1" name="dateOfJoining" placeholder="" />
+                        <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Date Of Official Joining *</label>
+                            <input autoComplete="off" onChange={handleInput} type="date" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="dateOfJoining" placeholder="" />
 
 
-                            <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Password *</label>
-                                <input autoComplete="off" onChange={handleInput} type="password" className="form-control" id="exampleFormControlInput1" name="password" placeholder="Password must be atleast 8 digits and create a strong , unpredectible password" />
+                            <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Password *</label>
+                                <input autoComplete="off" onChange={handleInput} type="password" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="password" placeholder="Password must be atleast 8 digits and create a strong , unpredectible password" />
                             </div>
 
-                            <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">Confirm Password *</label>
-                                <input autoComplete="off" onChange={handleInput} type="password" className="form-control" id="exampleFormControlInput1" name="cpassword" placeholder="" />
+                            <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">Confirm Password *</label>
+                                <input autoComplete="off" onChange={handleInput} type="password" className="form-control formInputRegistraion" id="exampleFormControlInput1" name="cpassword" placeholder="" />
                             </div>
 
 
 
-                            <div className="mb-3 techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label techStaffRegistrationLabel1">choose File *</label>
+                            <div className="mb-3 formDivRegistraion techStaffRegistrationLabel"> <label for="exampleFormControlInput1" className="form-label">choose File *</label>
                                 <input name="photograph" type="file" onChange={(e) => setImage(e.target.files[0])} />
                             </div>
 
